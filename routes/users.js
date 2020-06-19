c6704387e5368f13124db4519c75ed13073a74ce
Karen Okonkwo <karenokonkwo@Karens-MacBook-Pro.local>
2020-06-20 00:05:47 +0100

    Include logic to register user and return jwt token

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,61 @@
 const express = require("express");
-const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require('express-validator');
 
+const User = require("../models/User");
+
+const router = express.Router();
 
 // @route   api/users/register
 // @desc    Register a new user
 // @access  Public
-router.post("/register", (req, res) => res.send("Register a user"));
+router.post("/register", [
+    check("name", "No name provided.").not().isEmpty(),
+    check("email", "Enter a valid email.").isEmail(),
+    check("password", "Enter a password with 6 characters or more.").isLength({
+        min: 6
+    })
+],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { name, password, email } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+            if (user) return res.status(400).json({ msg: "A user with this email already exists." });
+
+            user = await User({ name, password, email });
+
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+
+            const jwtPayload = {
+                user: { id: user.id }
+            };
+
+            jwt.sign(
+                jwtPayload,
+                config.get("jwtSecret"),
+                {
+                    expiresIn: 3600
+                },
+                (err, token) => {
+                    console.error(err);
+                    return res.json({ token: token });
+                });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ msg: "Something went wrong on our end." });
+        }
+
+    });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const connectToDB = require("./config/db");
 
 const app = express();
 
+// Connect Database
+connectToDB();
+
+// Initialize middle ware to accept json response
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("Welcome to the Contact Keeper API"));
 
+// Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
