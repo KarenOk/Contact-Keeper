fdc3aad33536ec7306139e983f896f9a8fa94e1c
Karen Okonkwo <karenokonkwo@Karens-MacBook-Pro.local>
2020-06-26 02:38:11 +0100

    feat: include completed functionality for all endpoints

---
+++ models/Contact.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const ContactSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+    },
+    phone: {
+        type: String
+    },
+    type: {
+        type: String,
+        default: 'personal'
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = mongoose.model('contact', ContactSchema);
