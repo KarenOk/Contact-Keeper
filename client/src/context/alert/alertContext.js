c09fbc7279be838a34de621a9b7f032ea61f6ee4
Karen Okonkwo <karenokonkwo@Karens-MacBook-Pro.local>
2020-07-01 23:07:17 +0100

    remove unused imports

--- client/src/context/alert/alertContext.js
+++ client/src/context/alert/alertContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from "react";
+import { createContext, useContext } from "react";
 
 const alertContext = createContext();
 export const useAlertContext = () => useContext(alertContext);
